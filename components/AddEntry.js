f5fb5d3c22c0deadffd5ce8fa776a98d44939abc
Your Name <karolina.kantorska@gmail.com>
2020-01-23 14:21:11 +0100

    reset State onSubmit

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react"
-import { View, Text } from "react-native"
-import { getMetricMetaInfo } from "../utils/helpers"
+import { View, Text, TouchableOpacity } from "react-native"
+import { getMetricMetaInfo, timeToString } from "../utils/helpers"
 import UdaciSlider from "./UdaciSlider"
 import UdaciSteppers from "./UdaciSteppers"
 import DateHeader from './DateHeader'
+
+function SubmitBtn ({ onPress }) {
+  return (
+    <TouchableOpacity onPress= {onPress}>
+      <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  )
+}
 
 export default class AddEntry extends Component {
   state = {
@@ -37,6 +45,27 @@ export default class AddEntry extends Component {
       [metric]: value,
     }))
   }
+  submit = () => {
+    const key = timeToString()
+    const entry = this.state
+
+    // Update Redux
+
+    this.setState(() => ({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0,
+   }))
+
+    // Navigate to home
+
+    // Save to DB
+
+    // Clear Local Notifications
+
+  }
   render () {
     const metaInfo= getMetricMetaInfo()
 
@@ -65,6 +94,7 @@ export default class AddEntry extends Component {
             </View>
           )
         })}
+        <SubmitBtn onPress={this.submit}/>
       </View>
     )
   }
